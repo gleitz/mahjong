88f2950ac75cd90f58b5169e6efc5b547e3ab6bc
Benjamin Gleitzman <gleitz@mit.edu>
2017-01-07 09:27:14 -0800

    Better auth

--- server/db.js
+++ server/db.js
@@ -1,26 +1,24 @@
-/*global module*/
+/*global module process*/
 
 /*
  * Master connection to MongoDB
  */
 
-var MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server;
+var MongoClient = require('mongodb').MongoClient;
 
-require('dotenv').config({path: '../.env'})
+require('dotenv').config();
 
 module.exports.init = function (callback) {
-    var mongoClient = new MongoClient(new Server('localhost', 27017));
-    mongoClient.open(function(error, mongoClient) {
-        module.exports.client = mongoClient.db('mahjong');
-        module.exports.client.authenticate(process.env.DB_USER, process.env.DB_PASS, function() {
-            module.exports.players = module.exports.client.collection('players');
-            module.exports.games = module.exports.client.collection('games');
-        })
-        module.exports.session = mongoClient.db('session');
-        module.exports.session.authenticate(process.env.DB_USER, process.env.DB_PASS)
+    var user_and_pass = process.env.DB_USER + ":" + process.env.DB_PASS;
+    MongoClient.connect("mongodb://" + user_and_pass + "@localhost:27017/mahjong", function(err, mahjong_db) {
+        module.exports.client = mahjong_db;
+        module.exports.players = module.exports.client.collection('players');
+        module.exports.games = module.exports.client.collection('games');
+    });
+    MongoClient.connect("mongodb://" + user_and_pass + "@localhost:27017/session", function(err, session_db) {
+        module.exports.session = session_db;
         if (typeof(callback) == 'function') {
-            callback(error);
+            callback();
         }
     });
 };
