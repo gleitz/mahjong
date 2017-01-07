84410727aa1896602596ad1546b47c36b70a8ebf
Benjamin Gleitzman <gleitz@mit.edu>
2017-01-07 09:04:11 -0800

    Better auth

--- server/db.js
+++ server/db.js
@@ -13,11 +13,12 @@ module.exports.init = function (callback) {
     var mongoClient = new MongoClient(new Server('localhost', 27017));
     mongoClient.open(function(error, mongoClient) {
         module.exports.client = mongoClient.db('mahjong');
-        module.exports.client.authenticate(process.env.DB_USER, process.env.DB_PASS)
+        module.exports.client.authenticate(process.env.DB_USER, process.env.DB_PASS, function() {
+            module.exports.players = module.exports.client.collection('players');
+            module.exports.games = module.exports.client.collection('games');
+        })
         module.exports.session = mongoClient.db('session');
-        module.exports.client.authenticate(process.env.DB_USER, process.env.DB_PASS)
-        module.exports.players = module.exports.client.collection('players');
-        module.exports.games = module.exports.client.collection('games');
+        module.exports.session.authenticate(process.env.DB_USER, process.env.DB_PASS)
         if (typeof(callback) == 'function') {
             callback(error);
         }
