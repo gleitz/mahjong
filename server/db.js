1d2be9988d6efeff83cd330cae2ffeb450b9129f
Benjamin Gleitzman <gleitz@mit.edu>
2017-01-07 09:01:45 -0800

    Added dotenv for MongoClient connections

--- server/db.js
+++ server/db.js
@@ -7,11 +7,15 @@
 var MongoClient = require('mongodb').MongoClient,
     Server = require('mongodb').Server;
 
+require('dotenv').config()
+
 module.exports.init = function (callback) {
     var mongoClient = new MongoClient(new Server('localhost', 27017));
     mongoClient.open(function(error, mongoClient) {
         module.exports.client = mongoClient.db('mahjong');
+        module.exports.client.authenticate(process.env.DB_USER, process.env.DB_PASS)
         module.exports.session = mongoClient.db('session');
+        module.exports.client.authenticate(process.env.DB_USER, process.env.DB_PASS)
         module.exports.players = module.exports.client.collection('players');
         module.exports.games = module.exports.client.collection('games');
         if (typeof(callback) == 'function') {
